17d39cea4ed81c17647308c6ed42a6dc65e1b6a8
krapka_v_komi <lg2031832@gmail.com>
2022-09-02 23:16:03 +0300

    Planning stage 0.1

--- new/news/models.py
+++ new/news/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=150)
-    content = models.TextField()
-    created_ad = models.DateTimeField(auto_now_add=True)
-    updated_ad = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BinaryField(default=True)
+    is_published = models.BooleanField(default=True)

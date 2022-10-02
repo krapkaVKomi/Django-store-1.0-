32bacc32e0d82d1c181238d62d934c7a71e93e94
User <lg2031832@gmail.com>
2022-10-02 21:34:55 +0300

    admin ++

--- new/news/models.py
+++ new/news/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Назва новини')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публікації')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата оновлення')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    is_published = models.BooleanField(default=True, verbose_name='Опубліковано ?')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
+        ordering = ['-created_at', 'title']
